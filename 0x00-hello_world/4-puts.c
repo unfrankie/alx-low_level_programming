e5045c10be122915494bc54b94f21f1f3a6fa4b2
Anass El Omari <anas.iramol@gmail.com>
2023-09-07 06:49:20 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/*
+   *main > ENTRY POINT
+   *puts > Fancy way of printf
+   *return > classic
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
