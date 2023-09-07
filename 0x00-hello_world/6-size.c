4b3dc003ee0bdb21fed62b063795483c3083d0c1
Anass El Omari <anas.iramol@gmail.com>
2023-09-07 07:36:46 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-  * main - entry point 
+  * main - Entry point 
   * printf - display what within
-  * return: always 0 (success)
+  * Return: Always 0 (Success)
   */
 int main(void)
 {
