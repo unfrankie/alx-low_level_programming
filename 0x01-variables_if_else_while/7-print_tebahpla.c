d09b3ecbe2311cde2dc01d332fc495cf539ef2db
Anass El Omari <anas.iramol@gmail.com>
2023-09-08 08:21:40 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * description - alphabet in lower case
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
