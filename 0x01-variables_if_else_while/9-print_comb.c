f1999cd88efaabcbbc794a93cadec913ce4f623f
Anass El Omari <anas.iramol@gmail.com>
2023-09-08 08:58:51 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+  * main - Entry point
+  * description - display numbers v3.0
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
