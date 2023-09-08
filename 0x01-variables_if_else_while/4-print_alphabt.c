4c8247445371710f298e9a69ddc04099b9d76224
Anass El Omari <anas.iramol@gmail.com>
2023-09-08 07:41:00 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+  * main - Entry point
+  * description - alphabet but racist to e and q
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int n = 98;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
