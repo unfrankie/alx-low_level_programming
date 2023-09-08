39d3cee8e07de7a98053aaa604e4ea6bdb462b4c
Anass El Omari <anas.iramol@gmail.com>
2023-09-08 08:00:35 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+  * main - Entry point
+  * description - display numbers
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar('n');
+		n++;
+	}
+	putchar('\n');
+	return (0);
