57e9cf8bff7fff45a237c8063a11c36b06fc8cc4
Anass El Omari <anas.iramol@gmail.com>
2023-09-08 07:48:00 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+  * main - Entry point
+  * description - display numbers
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
