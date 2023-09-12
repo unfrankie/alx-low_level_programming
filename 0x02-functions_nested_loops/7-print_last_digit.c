8e3e328398c606800e1659f76e22414fa7185d03
Anass El Omari <anas.iramol@gmail.com>
2023-09-12 11:09:12 -0700

    There are only 3 colors, 10 digits, and 7 notes; its what we do with them thats important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_last_digit - display the last digit of a number
+  * @n: the number that will be moduled
+  * return: always 0 (success)
+  */
+
+print_last_digit(int n)
+{
+	int i = 10 % n;
+
+	return (i);
+	putchar('\n');
+}
