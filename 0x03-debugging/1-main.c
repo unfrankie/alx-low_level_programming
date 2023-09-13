a19bdad2954cc62c6f1e5260a395d9d6dc99e72f
Anass El Omari <anas.iramol@gmail.com>
2023-09-13 06:46:58 -0700

    like comment subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,14 +11,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	/* A warning about an infinite loop */
-	i = 0;
-	while (i < 10)
-	{
-		putchar(i);
-	}
-	/* the infinite loop we were warned about */
+	/**
+	  *
+	  *i = 0;
+	  *while (i < 10)
+	  *{
+	  *putchar(i);
+	  *}
+	  */
 	printf("Infinite loop avoided! \\o/\n");
-	/* oh! its somehow avoided? */
 	return (0);
 }
