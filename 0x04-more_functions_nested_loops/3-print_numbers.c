c468d4b95dc7d351064f215b9265cf42e6d65c86
Anass El Omari <anas.iramol@gmail.com>
2023-09-14 06:43:35 -0700

    the numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_numbers - display numbers from 0 to 9
+  */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
