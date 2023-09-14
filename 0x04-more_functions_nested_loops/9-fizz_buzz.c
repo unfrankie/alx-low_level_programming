2295295b9cca9b360ebdb6e13f2f2c4d5cc9c4aa
Anass El Omari <anas.iramol@gmail.com>
2023-09-14 14:21:50 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - detect multiples of 3 and 5 using fizzbuzz
+  * Return: 0
+  */
+int main(void)
+{
+	int num;
+
+	while (num++ < 100)
+
+	if ((num % 3 == 0) && (num % 5 == 0))
+	printf("FizzBuzz ");
+
+	else if ((num % 3) == 0)
+	printf("Fizz ");
+
+	else if ((num % 5) == 0)
+	{
+		if (num != 100)
+		printf("Buzz ");
+
+		else
+		printf("Buzz");
+	}
+
+	else
+	printf("%d ", num);
+
+	printf("\n");
+	return (0);
+}
