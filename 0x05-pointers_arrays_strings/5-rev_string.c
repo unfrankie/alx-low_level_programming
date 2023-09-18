aeded603ab052f7cdaa8425e57119413d0686d25
Anass El Omari <anas.iramol@gmail.com>
2023-09-18 09:49:38 -0700

    A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he propose

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -34,18 +34,3 @@ int _strlen(char *s)
 	i++;
 	return (i);
 }
-
-/**
-  *swap_int - swap the value of two integers
-  *@a: 1st integer
-  *@b: 2nd integer
-  *Return: 0
-  */
-
-void swap_int(int *a, int *b)
-{
-	int i = *a;
-
-	*a = *b;
-	*b = i;
-}
