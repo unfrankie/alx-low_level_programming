8a237296b6534797b87f1a4bc86aa58fb1512ebb
Anass El Omari <anas.iramol@gmail.com>
2023-09-18 08:23:00 -0700

    i can only go one way. ive not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,3 +14,19 @@ void print_rev(char *s)
 	_putchar(s[rev]);
 	_putchar('\n');
 }
+
+/**
+  * _strlen - returns the length of a string
+  * @s: the string
+  * Return: returns the lenght
+  */
+
+int _strlen(char *s)
+{
+	int n, i;
+
+	i = 0;
+	for (n = 0; s[n] != '\0'; n++)
+	i++;
+	return (i);
+}
