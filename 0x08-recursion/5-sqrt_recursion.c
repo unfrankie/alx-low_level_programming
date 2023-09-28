d7276022f798f535cc90844d5c63e1f684561b8d
Anass El Omari <anas.iramol@gmail.com>
2023-09-28 08:17:44 -0700

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _sqrt_recursion - natural square root pf a number
+  * @n: the number
+  * return: if number is negative return -1
+  */
+
+int _sqrt_recursion(int n);
+{
+	int i;
+
+	n = i * i;
+	if (n < 0)
+		return (-1);
+	else
+		return (i);
+}
