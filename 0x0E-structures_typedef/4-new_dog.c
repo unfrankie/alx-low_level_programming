1187130659ff32cc6517ae3b6d9376fabd5986fd
Anass El Omari <anas.iramol@gmail.com>
2023-10-10 12:25:36 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,11 +2,11 @@
 #include "dog.h"
 
 /**
-  * _strlen - returns the length of a string
-  * @s: the string
-  * Return: returns the lenght
-  */
-
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
 	int i;
@@ -22,26 +22,26 @@ int _strlen(char *s)
 }
 
 /**
-  * *_strcpy - copy the string pointed to by src
-  * including the terminating null byte (\0)
-  * to the buffer pointed to by dest
-  * @dest: pointer to the buffer in which we copy the string
-  * @src: string to be copied
-  * Return: the pointer to dest
-  */
-
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
 char *_strcpy(char *dest, char *src)
 {
-	int l, i;
+	int len, i;
 
-	l = 0;
+	len = 0;
 
-	while (src[l] != '\0')
+	while (src[len] != '\0')
 	{
-		l++;
+		len++;
 	}
 
-	for (i = 0; i < l; i++)
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
@@ -51,13 +51,13 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
-  * new_dog - create a new dog
-  * @name: name of the dog
-  * @age: age of the dog
-  * @owner: owner of the dog
-  * Return: pointer || NULL
-  */
-
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
