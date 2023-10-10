cb2ef4e7f053e7aa195f3c971c4dfae553e9cb37
Anass El Omari <anas.iramol@gmail.com>
2023-10-10 13:32:08 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,46 +2,42 @@
 #include "dog.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
+  * _strlen - returns the length of a string
+  * @s: the string
+  * Return: returns the lenght
+  */
+
 int _strlen(char *s)
 {
-	int i;
+	int n, i;
 
 	i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
+	for (n = 0; s[n] != '\0'; n++)
+	i++;
 	return (i);
 }
 
 /**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
- *
- * Return: the pointer to dest
- */
+  * *_strcpy - copy the string pointed to by src
+  * including the terminating null byte (\0)
+  * to the buffer pointed to by dest
+  * @dest: pointer to the buffer in which we copy the string
+  * @src: string to be copied
+  * Return: the pointer to dest
+  */
+
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	int l, i;
 
-	len = 0;
+	l = 0;
 
-	while (src[len] != '\0')
+	while (src[l] != '\0')
 	{
-		len++;
+		l++;
 	}
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < l; i++)
 	{
 		dest[i] = src[i];
 	}
@@ -51,13 +47,13 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog (Success), NULL otherwise
- */
+  * new_dog - create a new dog
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: owner of the dog
+  * Return: pointer || NULL otherwise
+  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
