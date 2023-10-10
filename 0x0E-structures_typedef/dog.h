ffc93d70fa370de8fcd2780782d34366dba70f32
Anass El Omari <anas.iramol@gmail.com>
2023-10-10 05:39:54 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+  * struct dog - dog info
+  * @name: 1st element
+  * @age: 2nd element
+  * @owner: 3rd element
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
