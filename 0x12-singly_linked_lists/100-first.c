e208196fffe9568d9948e63568883e9747a3c2e6
Anass El Omari <anas.iramol@gmail.com>
2023-10-19 09:23:13 -0700

    fixing The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
-void before(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
- * before - print You're beat! and yet, you must allow,
- * I bore my house upon my back! 
- * before the main function is executed
- */
+  * first - print You're beat! and yet, you must allow,
+  * I bore my house upon my back!
+  * before the main function is executed
+  */
 
-void before(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
