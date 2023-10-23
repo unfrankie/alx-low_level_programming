fa1d2edaa280ca196f3520dfdf6ee798fe7eb7e0
Anass El Omari <anas.iramol@gmail.com>
2023-10-23 04:55:38 -0700

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
+void first(void) __attribute__ ((constructor));
+
 /**
   * first - print You're beat! and yet, you must allow,
   * I bore my house upon my back!
   * before the main function is executed
   */
-
-void first(void) __attribute__ ((constructor));
 
 void first(void)
 {
