29fd30ef8ee51cacf31e48cc3ec8d5684fb48c03
Anass El Omari <anas.iramol@gmail.com>
2023-10-30 09:24:15 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,30 +19,30 @@ int cp_file(int argc, char *argv[])
 	}
 	c = filesize(argv[2]);
 	src = open(argv[1], O_RDONLY);
-	r = read(src, filename, 1024);
+	r = read(src, c, 1024);
 	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while (r > 0)
 	{
 		if (src == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO,"Error: Can't read from file %s\n
-					", argv[1]);
-			free(filename);
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			free(c);
 			exit(98);
 		}
-		w = write(dest, filename, r);
+		w = write(dest, c, r);
 		if (dest == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO,"Error: Can't write to %s\n", 
-					argv[2]);
-			free(filename);
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
+			free(c);
 			exit(99);
 		}
-		r = read(src, filename, 1024);
+		r = read(src, c, 1024);
 		dest = open(argv[2], O_WRONLY | O_APPEND);
 	}
-	free(filename);
-	close_file(src);
-	close_file(dest);
+	free(c);
+	closefile(src);
+	closefile(dest);
 	return (0);
 }
