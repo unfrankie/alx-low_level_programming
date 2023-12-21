9a99328f97fd40c134bb1588e090eccfdc09188b
unfrankie <141495574+unfrankie@users.noreply.github.com>
2023-12-21 20:08:19 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,15 +25,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(temp->value);
 			temp->value = strdup(value);
 			if (temp->value == NULL)
-				return 0;
-			return 1;
+				return (0);
+			return (1);
 		}
 		temp = temp->next;
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return 0;
+		return (0);
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
@@ -42,11 +42,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node->key);
 		free(new_node->value);
 		free(new_node);
-		return 0;
+		return (0);
 	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
-	return 1;
+	return (1);
 }
