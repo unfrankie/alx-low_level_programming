f939810da3a2daacd9bc48b0510afb61ff4baaad
Anass El Omari <anas.iramol@gmail.com>
2024-04-09 06:15:46 -0700

    Create directory - Create readme file - Implement linear and binary search algorithms, along with Big O notation solutions

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGO_header
+#define SEARCH_ALGO_header
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
